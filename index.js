7d7a3b86c7aba2f0a4283aee75be1d85dd108b13
Fennixs <barutf1500@gmail.com>
2024-09-20 14:07:35 -0400

    cleaned up bugs

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 // Get the display element
 const display = document.getElementById("display");
 
-// Variable to store the memory value
-let memoryValue = 0;
-
 /**
  * Appends the given input to the display value.
  * @param {string} input - The input to append to the display.
@@ -29,23 +26,5 @@ function calculate() {
  */
 function clearDisplay() {
     display.value = "";
-}
-
-/**
- * Adds the current display value to the memory value and updates the display.
- * TODO: Debug why appendToDisplayAdd is not working as expected
- * Possible issues to check:
- * 1. Ensure the button calling this function has the correct onclick attribute.
- * 2. Verify that display.value contains a valid number before parsing.
- * 3. Add pi, % and +/- operations 
- */
-function appendToDisplayAdd() {
-    // Check if display value is not empty
-    if (display.value !== "") {
-        // Parse the display value and add it to memoryValue
-        memoryValue += parseFloat(display.value);
-        // Update the display to show the new memory value
-        display.value = memoryValue;
-    }
 }
 
